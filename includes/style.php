4be4764f4488fb0ef4ff7cc67df09132c3727c9e
faheem456 <46492824+sharjeelarshad014@users.noreply.github.com>
2024-05-12 00:04:53 +0500

    ahad for you

--- includes/style.php
+++ includes/style.php
@@ -1,6 +1,7 @@
 <link rel="icon" type="image/x-icon" size="16x16" href="/favicon.ico?v=<?php echo "$cversion";?>">
 <link rel="preload" href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="/junaid1/assets/css/style.min.css?v=<?php echo "$cversion";?>"/>
+<link rel="stylesheet" type="text/css" href="/junaid1/assets/css/style.min2.css"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css"/>
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <!-- Google tag (gtag.js) -->
