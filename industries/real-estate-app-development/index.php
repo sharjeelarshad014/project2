9824a3acddf1e9f00d2d7ec7bcf4057c2e4fb0c2
Abdul Ahad <97602347+abdulahad0311@users.noreply.github.com>
2024-06-25 21:32:03 +0500

    mm

--- industries/real-estate-app-development/index.php
+++ industries/real-estate-app-development/index.php
@@ -187,29 +187,7 @@
         </div>
     </section>
 
-    <section class="stripe project-sec">
-		<div class="container">
-			<div class="project-wrap">
-				<div class="row align-items-center justify-content-between">
-					<div class="col-lg-6">
-						<div class="project-content">
-							<h3 data-aos="fade-right">Let's Discuss Your Project</h3>
-							<p data-aos="fade-right"> Get Free Consultation And Let Us Know Your Project Idea To Turn It Into An Amazing Digital Product.</p>
-							<div class="project-btn">
-								<a href="/contact-us.php" class="btn btn-white" data-aos="fade-right" data-aos-duration="500">Get a Quote</a>
-								<a href="javascript:;" class="btn btn-black" data-aos="fade-right" data-aos-duration="1000">Live Chat</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4">
-						<div class="project-img" data-aos="zoom-out-left">
-							<img src="/junaid1/assets/img/mix/why-choose-img.avif" alt="">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+    <?php include($_SERVER['DOCUMENT_ROOT']."/junaid1/includes/cta.php"); ?>
 
     <section class="stripe box-sec">
         <div class="container">
